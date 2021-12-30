9aba0eb695cf548953a7098ca021fb75d52ec0d0
chefvivica <vivicacorn@gmail.com>
2021-12-30 00:26:05 -0500

    updates for BFS solution

--- breadth_first_values.py
+++ breadth_first_values.py
@@ -1,18 +1,22 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
 from collections import deque
 def breadth_first_values(root):
-  if root is None:
-    return []
-  queue = deque([ root ])
-  result = [ ]
-  while queue:
-    current = queue.popleft()
-    result.append(current.val)
-    if current.left is not None and current.right is not None:
-      queue.append(current.left)
-      queue.append(current.right)
-    if current.left is not None and current.right is None:
-      queue.append(current.left)
-    if current.right is not None and current.left is None:
-      queue.append(current.right)
-  return result
+    if root is None:
+        return []
+    queue = deque([ root ])
+    result = [ ]
+    while queue:
+        current = queue.popleft()
+        result.append(current.val)
+        if current.left:
+            queue.append(current.left)
+
+        if current.right:
+            queue.append(current.right)
+    return result
   
